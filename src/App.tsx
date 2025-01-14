013f6df7896fe6c4f0370b5c0b5a23d545acc448
Stefan Bloom <stefanjbloom88@gmail.com>
2025-01-13 19:15:36 -0700

    feat: home screen, react router, typed.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import icon from '/icon.jpeg'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
+// import Education from './components/Education'
+// import Skills from './components/Skills'
+// import About from './components/About'
+// import Projects from './components/Projects'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={icon} className="logo" alt="home logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-gray-100 text-gray-800">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,54 @@
+// import Navbar from  './NavBar';
+import endpoints from '../endpoints/endpoints';
+import React, { useState, useEffect, useRef} from 'react';
+import Typed from 'typed.js';
+
+function Home () {
+
+  const [data, setData] = useState<{ name: String; roles: string[] } | null>(null)
+  const typedElement = useRef<HTMLSpanElement>(null)
+  const currentElement = useRef<Typed | null>(null)
+
+  useEffect(() => {
+    fetch(endpoints.home, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.error('Error Fetching Home Data: ', err));
+  }, []);
+
+  useEffect(() => {
+    if(data && typedElement.current) {
+      currentElement.current = new Typed(typedElement.current, {
+        strings: data.roles,
+        typeSpeed: 70,
+        backSpeed: 40,
+        backDelay: 700,
+        loop: true
+      })
+    }
+    return () => {
+      if(currentElement.current){
+        currentElement.current.destroy()
+      }
+    };
+  }, [data])
+
+  return (
+    <div className='min-h-screen bg-gray-100'>
+      {/* <Navbar /> */}
+      <div className='flex flex-col items-center justify-center h-screen w-screen text-center'>
+        {data && (
+          <>
+          <h1 className='text-4x1 font-bold mb-4'>{data.name}</h1>
+          <h2 className='text-2x1'>
+            I am <span ref={typedElement} className='text-blue-500' />
+          </h2>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+export default Home
