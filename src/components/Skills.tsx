952339dafccf18b1593943ecfef55da0c432d7b2
Stefan Bloom <stefanjbloom88@gmail.com>
2025-01-15 19:37:43 -0700

    Feat:skills styled and g2g

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -32,36 +32,36 @@ function Skills() {
   return (
     <div className="min-h-screen min-w-screen bg-gradient-to-b from-gray-100 via-white to-gray-350">
       <Navbar />
-      <div className="flex flex-row items-center justify-center gap-5 pxx-4 lg:px-20">
-        {data && (
-          <>
-            <header className="font-serif text-lg text-gray-900 mt-5 leading-relaxed text-center font-bold">
+      {data && (
+        <>
+          <header className="text-center mt-8 px-4">
+            <h1 className="font-serif text-lg text-gray-900 leading-relaxed font-bold max-w-3xl mx-auto">
               {data.intro}
-            </header>
-            <div className="font-serif text-lg text-gray-900 mt-5 leading-relaxed text-center font-bold max-w-3xl">
-              {data.skills.map((skill, index) => (
-                <div key={index} className="bg-white shadow-md rounded-lg p-4">
-                  <h2 className="text-xl font-semibold text-gray-800 mb-4 text-center">
-                    {skill.title}
-                  </h2>
-                  <ul className="space-y-2">
-                    {skill.items.map((item, id) => (
-                      <li key={id} className="flex items-center gap-3">
-                        <img
-                          src={item.icon}
-                          alt={item.title}
-                          className="w-8 h-8 object-contain"
-                        />
-                        <span className="text-gray-700">{item.title}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </div>
-          </>
-        )}
-      </div>
+            </h1>
+          </header>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-10 px-4 lg:px-20">
+            {data.skills.map((skill, index) => (
+              <div key={index} className="bg-white shadow-md rounded-lg p-4">
+                <h2 className="text-xl font-semibold text-gray-800 mb-4 text-center">
+                  {skill.title}
+                </h2>
+                <ul className="space-y-2">
+                  {skill.items.map((item, id) => (
+                    <li key={id} className="flex items-center gap-3">
+                      <img
+                        src={item.icon}
+                        alt={item.title}
+                        className="w-8 h-8 object-contain"
+                      />
+                      <span className="text-gray-700 font-serif">{item.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
